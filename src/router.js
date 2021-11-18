805dd966b047865d194c7807c87b8eae34721ce5
kibroman <you@example.comkibromanx@gmail.com>
2021-11-17 22:44:19 -0800

    updated

--- src/router.js
+++ src/router.js
@@ -1,25 +1,32 @@
-import { createWebHistory, createRouter } from "vue-router";
+import {
+    createWebHistory,
+    createRouter
+} from "vue-router";
 // import Home from '@/views/Home.vue';
 // import Samples from '@/view/Samples.vue';
 const Home = () => import('./views/Home.vue');
 const Samples = () => import('./views/Samples.vue');
 
-const history = createWebHistory();
+
 
 const router = createRouter({
-    history,
-    routes: [{
-        path: "/",
-        name: 'Home',
-        component: Home,
-    },
-    {
-        path: "/Samples",
-        name: 'Samples',
-        component: Samples,
-    }
-]
-    
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/', redirect: '/home'
+        },
+        {
+            path: "/home",
+            name: 'Home',
+            component: Home,
+        },
+        {
+            path: "/samples",
+            name: 'Samples',
+            component: Samples,
+        }
+    ]
+
 })
 
 export default router;
